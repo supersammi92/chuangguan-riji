c84307949c57f7579b0bac5367344c68837c61c2
supersammi92 <supersammi@163.com>
2026-08-17 16:01:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for 小六一的闯关日记 PWA — v8.6
-const CACHE_NAME = 'chuangguan-riji-v86';
+// Service Worker for 小六一的闯关日记 PWA — v8.9
+const CACHE_NAME = 'chuangguan-riji-v89';
 const ASSETS = [
   '/',
   '/daily-workbench.html',
